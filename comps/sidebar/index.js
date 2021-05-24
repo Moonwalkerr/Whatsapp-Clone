65b40d8468b58686341b0d68b2bd7cb652e0cf27
Moonwalker <abhisk.mhra@gmail.com>
2021-05-24 21:45:20 +0530

    Sidebar Componet : added header, searchbox and sidebar chat display containers

--- comps/sidebar/index.js
+++ comps/sidebar/index.js
@@ -1,4 +1,4 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar, Button, IconButton } from "@material-ui/core";
 import { ChatBubbleTwoTone, MoreVert, SearchSharp } from "@material-ui/icons";
 import styled from "styled-components";
 
@@ -8,14 +8,20 @@ const Sidebar = () => {
       <Sidebar_Header>
         <Sidebar_HeaderAvatar />
         <Sidebar_HeaderIcons>
-          <Sidebar_HeaderChat />
-          <Sidebar_HeaderMenu />
+          <IconButton>
+            <Sidebar_HeaderChat />
+          </IconButton>
+          <IconButton>
+            <Sidebar_HeaderMenu />
+          </IconButton>
         </Sidebar_HeaderIcons>
       </Sidebar_Header>
       <Sidebar_SearchContainer>
         <Sidebar_SearchIcon />
         <Sidebar_SearchBox />
       </Sidebar_SearchContainer>
+      <Sidebar_AddNewChat>Start a new chat!</Sidebar_AddNewChat>
+      <Sidebar_ChatsDisplay></Sidebar_ChatsDisplay>
     </Sidebar_Container>
   );
 };
@@ -26,12 +32,43 @@ export default Sidebar;
 const Sidebar_Container = styled.div``;
 
 // this is sidebar header : will contain user-avatar and primary buttons
-const Sidebar_Header = styled.div``;
-const Sidebar_HeaderAvatar = styled(Avatar);
-const Sidebar_HeaderIcons = styled.div``;
+const Sidebar_Header = styled.div`
+  flex: 1;
+  display: flex;
+  flex-flow: row nowrap;
+  background-color: whitesmoke;
+  padding: 12px;
+  align-items: center;
+  justify-content: space-between;
+  position: sticky;
+  box-shadow: 1px 3px 3px 2px rgba(0, 0, 0, 0.2);
+`;
+const Sidebar_HeaderAvatar = styled(Avatar)``;
+const Sidebar_HeaderIcons = styled.div`
+  display: flex;
+  align-items: center;
+`;
 const Sidebar_HeaderChat = styled(ChatBubbleTwoTone)``;
 const Sidebar_HeaderMenu = styled(MoreVert)``;
 
-const Sidebar_SearchContainer = styled.div``;
+const Sidebar_SearchContainer = styled.div`
+  flex: 1;
+  margin: 12px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Sidebar_SearchIcon = styled(SearchSharp)``;
-const Sidebar_SearchBox = styled.input``;
+const Sidebar_SearchBox = styled.input`
+  width: 80%;
+  font-size: 20px;
+  outline: 0;
+`;
+
+// Start new chat Button
+const Sidebar_AddNewChat = styled(Button)`
+  width: 100%;
+`;
+
+// Sidebar Chats Display Container
+const Sidebar_ChatsDisplay = styled.div``;
